b61641282a25532aaf96ed6a2524080dfe9d2926
swabhiman raj <123272965+swabhiman2005@users.noreply.github.com>
2023-04-01 05:03:40 +0530

    Add files via upload

---
+++ write a program to print the following pattern right hand print hello word.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int i,w,p;
+    char str[] = "HELLO";
+    printf("\n");
+    for(i=0; i<=5; i++)
+    {
+        p=i+1;
+        printf("\n %5.*s",p,str);
+    }
+    for(i=5; i>=0; i--)
+    {
+        p = i+1;
+        printf("\n %5.*s",p,str);
+    }
+    getch();
+    return 0;
+}
